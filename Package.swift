4d0df0fa1a6a09b237e73fc4042fd03f60298861
Shaunak Jagtap <153058544+blitzllama-ios@users.noreply.github.com>
2024-01-18 21:20:55 +0530

    release: 1.6.0

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
 // swift-tools-version:5.5
 import PackageDescription
 
-let remoteUrl = "https://github.com/Blitzllama/ios-releases/archive/refs/tags/1.5.8.zip"
+let remoteUrl = "https://github.com/Blitzllama/ios-releases/archive/refs/tags/1.6.0.zip"
 let remoteChecksum = "63765bfd05150cc516c98fd8d5e408d30bec845a60383ff93382e6ad8681b19d"
 let packageName = "BlitzLlama"
 
 let package = Package(
     name: "BlitzLlama",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(name: packageName, targets: [packageName])
     ],
