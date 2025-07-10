f9d08336f91e869de51869edc68bd95e2a52d4be
Sudhanshu Dwivedi <sudhanshu.dwivedi@astrotalk.com>
2025-07-10 21:48:52 +0530

    Sdk version updated to 1.6.36

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.5
 import PackageDescription
 
-let remoteUrl = "https://github.com/Blitzllama/ios-releases/archive/refs/tags/1.6.32.zip"
-let remoteChecksum = "fb2f058d0da93bdbfe086fab9989df968a330e68c9c2291b0ef243555f22be50"
+let remoteUrl = "https://github.com/Blitzllama/ios-releases/archive/refs/tags/1.6.35.zip"
+let remoteChecksum = "1080e5e30a030b3666f84ffa6f6293e8af57c885bc7cbd46613b1c942b2de2d7"
 let packageName = "BlitzLlama"
 
 let package = Package(
